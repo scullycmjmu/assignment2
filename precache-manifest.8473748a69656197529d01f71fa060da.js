c9605945f1f0acf67718b46706917fd036244be5
scullycmjmu <christianscully@Christians-Air.hsd1.nj.comcast.net>
2020-03-19 11:58:17 -0400

    Updates

---
+++ precache-manifest.8473748a69656197529d01f71fa060da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4c848b9616a7187ab22040a246f00b",
+    "url": "/assignment2/index.html"
+  },
+  {
+    "revision": "d3b90d6d57722196a1ca",
+    "url": "/assignment2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e35e90ebdfad0ae1082b",
+    "url": "/assignment2/static/js/2.d1bab822.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/assignment2/static/js/2.d1bab822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b90d6d57722196a1ca",
+    "url": "/assignment2/static/js/main.567243dd.chunk.js"
+  },
+  {
+    "revision": "f68b64317dd5aae840ab",
+    "url": "/assignment2/static/js/runtime-main.d0e18a45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assignment2/static/media/logo.5d5d9eef.svg"
+  }
+]);
