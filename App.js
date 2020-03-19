f234ef8afd27b99c1857bfce26588fc7677dd2fc
scullycmjmu <54854695+scullycmjmu@users.noreply.github.com>
2020-03-19 12:06:25 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Shoeinfo from './Shoeinfo';
+
+class App extends Component {
+  state = {
+    moreShoes:[
+      {brand:"Adidas", type:"Zoom", size:13},
+    ]
+  }
+  changeShoe = (updatedType) => {
+    this.setState({
+      moreShoes:[
+        {brand:"Puma",type: "Bolt", size:14},
+      ]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Christian Scully</h1>
+        <Shoeinfo brand="Nike" type="Lebron 9" size="13"/>
+        <Shoeinfo brand="Jordan" type="Zion 1" size="11"/>
+
+        <h1>I have many shoes!</h1>
+        <Shoeinfo brand={this.state.moreShoes[0].brand} type={this.state.moreShoes[0].type} size={this.state.moreShoes[0].size}/>
+        <button onClick={this.changeShoe}>Click to see hidden shoe</button>
+      </div>
+
+    );
+  }
+}
+
+export default App;
